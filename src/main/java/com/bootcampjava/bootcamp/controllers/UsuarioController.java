6437b8d4d33bf99cca126e2f6b9fc206aa272817
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-02-05 15:55:59 +0100

    call to API with AJAX with Stock

--- src/main/java/com/bootcampjava/bootcamp/controllers/UsuarioController.java
+++ src/main/java/com/bootcampjava/bootcamp/controllers/UsuarioController.java
@@ -1,17 +1,99 @@
 package com.bootcampjava.bootcamp.controllers;
 
+import com.bootcampjava.bootcamp.models.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
     // Request Mapping con la ruta test
-    @RequestMapping(value = "test")
-    public List<String> test() {
-        return List.of("test1", "test2", "test3");
+    @RequestMapping(value = "user/{id}")
+    public User getUser(@PathVariable Long id) {
+        User user = new User();
+        user.setId(213L);
+        user.setName("Juan");
+        user.setLastname("Perez");
+        user.setEmail("juan@juan.com");
+        user.setPassword("123456");
+        user.setPhone("123456789");
+
+        return user;
+    }
+
+    @RequestMapping(value = "users")
+    public List<User> getUsers() {
+        List<User> userList = new ArrayList<>();
+        User user = new User();
+        user.setId(0L);
+        user.setName("Juan");
+        user.setLastname("Gomez");
+        user.setEmail("juan@gomez.com");
+        user.setPassword("a123");
+        user.setPhone("6729292");
+
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setName("Jhon");
+        user1.setLastname("Stevens");
+        user1.setEmail("Jhon@Stevens.com");
+        user1.setPassword("e123");
+        user1.setPhone("123123123");
+
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("María");
+        user2.setLastname("Gonzalez");
+        user2.setEmail("Maria@Gonzalez.com");
+        user2.setPassword("i123");
+        user2.setPhone("123123123");
+
+        userList.add(user);
+        userList.add(user1);
+        userList.add(user2);
+
+        return userList;
+
+    }
+
+    @RequestMapping(value = "user1")
+    public User editUser() {
+        User user1 = new User();
+        user1.setName("Juan");
+        user1.setLastname("Perez");
+        user1.setEmail("juan@juan.com");
+        user1.setPassword("123456");
+        user1.setPhone("123456789");
+
+        return user1;
+    }
+
+    @RequestMapping(value = "user11")
+    public User deleteUser() {
+        User user1 = new User();
+        user1.setName("Juan");
+        user1.setLastname("Perez");
+        user1.setEmail("juan@juan.com");
+        user1.setPassword("123456");
+        user1.setPhone("123456789");
+
+        return user1;
+    }
+
+    @RequestMapping(value = "user111")
+    public User searchUser() {
+        User user1 = new User();
+        user1.setName("Juan");
+        user1.setLastname("Perez");
+        user1.setEmail("juan@juan.com");
+        user1.setPassword("123456");
+        user1.setPhone("123456789");
+
+        return user1;
     }
 
 
---
+++ src/main/java/com/bootcampjava/bootcamp/models/User.java
@@ -0,0 +1,59 @@
+package com.bootcampjava.bootcamp.models;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
