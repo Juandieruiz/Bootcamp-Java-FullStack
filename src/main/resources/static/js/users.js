d8e82a33071ff5ba5553cad633b819be5f48886c
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-02-05 15:59:43 +0100

    update users.js

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -1,31 +1,31 @@
-// Una vez que este cargada la página
-$(document).ready(function() {
-    // Carga la Tabla de Usuarios
-    cargarUsuarios();
-  $('#usersTable').DataTable();
-});
-
-// funcion asincrona para cargar la tabla de usuarios
-async function cargarUsuarios(){
-
-    // el await espera a que termine la funcion
-    const request = await fetch('users', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+    // Una vez que este cargada la página
+    $(document).ready(function() {
+        // Carga la Tabla de Usuarios
+        cargarUsuarios();
+      $('#usersTable').DataTable();
     });
 
-    const users = await request.json();
+    // funcion asincrona para cargar la tabla de usuarios
+    async function cargarUsuarios(){
 
-    let listHtml = '';
-    for (let user of users) {
-            let userHtml = '<tr><td>'+ user.id +'</td><td>' + user.name + ' ' + user.lastname + '</td><td>'
-                                + user.email + '</td><td>' + user.phone
-                                + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
-            listHtml += userHtml;
+        // el await espera a que termine la funcion
+        const request = await fetch('users', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const users = await request.json();
+
+        let listHtml = '';
+        for (let user of users) {
+                let userHtml = '<tr><td>'+ user.id +'</td><td>' + user.name + ' ' + user.lastname + '</td><td>'
+                                    + user.email + '</td><td>' + user.phone
+                                    + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+                listHtml += userHtml;
+        }
+
+        document.querySelector('#usersTable tbody').outerHTML = listHtml;
     }
-
-    document.querySelector('#usersTable tbody').outerHTML = listHtml;
-}
