5a9b57bd90a1df98bc5a62aa24c6c1661709fd78
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-04-22 12:21:26 +0200

    Finish Front & Test

--- src/main/java/com/bootcampjava/bootcamp/controllers/UsuarioController.java
+++ src/main/java/com/bootcampjava/bootcamp/controllers/UsuarioController.java
@@ -3,11 +3,15 @@ package com.bootcampjava.bootcamp.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @RequestMapping(value = "test")
-    public String test() {
-        return "Sending Test";
+    public List<String> test() {
+        return List.of("test", "test2", "test3");
     }
+
+
 }
