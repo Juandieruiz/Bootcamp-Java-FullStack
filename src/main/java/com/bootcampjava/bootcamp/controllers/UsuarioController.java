64cfe4d13508d6e978fd3d101a4846b2d33cfd22
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-04-28 13:07:46 +0200

    update project

--- src/main/java/com/bootcampjava/bootcamp/controllers/UsuarioController.java
+++ src/main/java/com/bootcampjava/bootcamp/controllers/UsuarioController.java
@@ -8,9 +8,10 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
+    // Request Mapping con la ruta test
     @RequestMapping(value = "test")
     public List<String> test() {
-        return List.of("test", "test2", "test3");
+        return List.of("test1", "test2", "test3");
     }
 
 
