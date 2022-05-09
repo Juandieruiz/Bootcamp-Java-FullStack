aa47e00f6496ebd575878c0118d307c1768b1692
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-05-09 11:53:37 +0200

    impl update

--- src/main/java/com/bootcampjava/bootcamp/dao/UserDaoImpl.java
+++ src/main/java/com/bootcampjava/bootcamp/dao/UserDaoImpl.java
@@ -5,12 +5,17 @@ import com.bootcampjava.bootcamp.models.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 //Patrón de diseño
-@Repository
-@Transactional
+@Repository // Repositorio de Base de Datos
+@Transactional // Consultas de Transacción SQL(en fragmentos)
 public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext // Contexto de Persistencia en la BBDD
+    private EntityManager entityManager; // Nos ayudara hacer la conexión con la BBDD
 
     @Override
     public List<User> getAllUsers() {
